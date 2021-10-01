0e69f140490b51223997e0c9b0aca96a6d881eac
dudaskobende <dudaskobende@gmail.com>
2021-10-01 22:59:12 +0200

    bende

--- Secret_projetc.py
+++ Secret_projetc.py
@@ -15,13 +15,13 @@ def main():
         elif link == 2:
             print("2: Nem, de szerintem most passzolom :)".center(middle()))
         else:
-            print("2: Nem".center(middle()))
+            print("2: Nem.".center(middle()))
 
     def middle():
         return shutil.get_terminal_size().columns
 
     def quit():
-        print("\u001b[31;1mAkkor legyen szép hétvégéd :)\033[0m".center(middle()))
+        print("Akkor legyen szép hétvégéd :)".center(middle()))
         sys.exit()
 
     def clear():
@@ -29,15 +29,15 @@ def main():
 
     def nick_name():
         clear()
-        print("\u001b[34;1mMi a codecool beceneved?\033[0m".center(middle()))
-        name = input(" " * (int(middle() / 2) - 5))
+        print("Mi a codecool beceneved?".center(middle()))
+        name = input(" " * (int(middle() / 2)))
         return name
 
     def movie_or_not():
         clear()
-        print("\u001b[34;1mSzeretsz moziba járni?\033[0m".center(middle()))
+        print("Szeretsz moziba járni?".center(middle()))
         yes_no(link=0)
-        first_choice = input(" " * (int(middle() / 2) - 5))
+        first_choice = input(" " * (int(middle() / 2)))
         clear()
         if first_choice == "2":
             print(':"('.center(middle()))
@@ -51,21 +51,21 @@ def main():
 
     def seen_or_not():
         clear()
-        print('\u001b[34;1mLáttad a Free Guy-t?\033[0m'.center(middle()))
+        print('Láttad a Free Guy-t?'.center(middle()))
         yes_no(link=1)
-        choice = input(" " * (int(middle() / 2) - 5))
+        choice = input(" " * (int(middle() / 2)))
         clear()
         if choice == "2":
             quit()
         elif choice == "3":
-            print("\u001b[34;1mJuhééééé\033[0m".center(middle()))
+            print("Juhééééé".center(middle()))
             time.sleep(1)
             print("Paszolom neked a magyar tarilert :)...".center(middle()))
             time.sleep(2)
             webbrowser.open('https://www.youtube.com/watch?v=1xGs9fhzQoI')
             time.sleep(2)
             print("Ha megnézted, csapj egy entert:)".center(middle()))
-            input(" " * (int(middle() / 2) - 5))
+            input(" " * (int(middle() / 2)))
             choice = "1"
             return choice
 
@@ -78,9 +78,9 @@ def main():
 
     def again_or_not():
         clear()
-        print('\u001b[34;1mVan kedved megnézni szombaton velünk moziban?\033[0m'.center(middle()))
+        print('Van kedved megnézni szombaton velünk moziban?'.center(middle()))
         yes_no(link=3)
-        wanna_watch_it = input(" " * (int(middle() / 2) - 5))
+        wanna_watch_it = input(" " * (int(middle() / 2)))
         if wanna_watch_it == "2":
             quit()
         elif wanna_watch_it == "1":
@@ -94,7 +94,7 @@ def main():
         print("Egyedül jönnél, vagy hoznál valakit?".center(middle()))
         print("1: Most egyedül".center(middle()))
         print("2: Ketten jönnénk".center(middle()))
-        partner = input(" " * (int(middle() / 2) - 5))
+        partner = input(" " * (int(middle() / 2)))
         if partner != '1' and partner != '2':
             print("mellé nyomtál. Próbáljuk meg újra :)".center(middle()))
             time.sleep(2)
@@ -107,11 +107,11 @@ def main():
 
     def which_saturday():
         clear()
-        print("Melyik szombat lenne jó Neked?".center(middle()))
-        print("1: Most szombat(október 2)".center(middle()))
+        print("Melyik nap lenne jó Neked?".center(middle()))
+        print("1: Most vasárnap(október 3)".center(middle()))
         print("2: Jövő hét szombat(október 9)".center(middle()))
-        print("3: Mind 2 szombat jó nekem".center(middle()))
-        saturday = input(" " * (int(middle() / 2) - 5))
+        print("3: Mind 2 időpont jó nekem".center(middle()))
+        saturday = input(" " * (int(middle() / 2)))
         if saturday != "1" and saturday != "2" and saturday != "3":
             print("mellé nyomtál. Próbáljuk meg újra :)".center(middle()))
             time.sleep(2)
@@ -119,15 +119,15 @@ def main():
         if saturday == "1":
             saturday = "Jövő hét szombat(október 9)"
         elif saturday == "3":
-            saturday = "Mind 2 szombat jó nekem"
+            saturday = "Mind 2 időpont jó nekem"
         else:
-            saturday = "Most szombat(október 2)"
+            saturday = "Most vasárnap(október 3)"
         return saturday
 
     def which_date():
         def names_dates():
             variable = []
-            with open('list.txt', 'r') as file:
+            with open('.secret_list/list.txt', 'r') as file:
                 menu = file.read().splitlines()
                 for line in menu:
                     variable.append(line)
@@ -135,20 +135,21 @@ def main():
 
         clear()
         variable = names_dates()
-        print('\u001b[34;1mTöbbiek, akik jönnek:\033[0m'.center(middle()))
+        print('Többiek, akik jönnek:'.center(middle()))
         for line in variable:
+            print("\n")
             print(f'{line}'.center(middle()))
         print('\n\n')
-        print('\u001b[34;1mNeked melyik időpont lenne a jó?\n\033[0m'.center(middle()))
+        print('Neked melyik időpont lenne a jó?'.center(middle()))
         print('Westend:'.center(middle()))
         print('1: 12:40'.center(middle()))
         print('2: 15:00'.center(middle()))
         print('\n\n')
-        print('Mammut:(ott kerekesszékkel segítség nelkül bejutok a terembe)'.center(middle()))
+        print('Mammut:'.center(middle()))
         print('3: 17:30'.center(middle()))
         print('\n\n')
         print("Egyszerre többet is megadhatsz, pl.: 123, vagy 12".center(middle()))
-        date = input(" " * (int(middle() / 2) - 5))
+        date = input(" " * (int(middle() / 2)))
         for string in date:
             if string != "1" and string != "2" and string != "3":
                 print("mellé nyomtál. Próbáljuk meg újra :)".center(middle()))
@@ -169,9 +170,9 @@ def main():
 
     def appender(nickname, partner, date, saturday):
         new_line = ("\n")
-        with open("list.txt", "a") as válasz:
-            válasz = open("list.txt", "a")
-            válasz.write(f'{nickname.capitalize()}: {partner}, {saturday},{" | ".join(date)}')
+        with open(".secret_list/list.txt", "a") as válasz:
+            válasz = open(".secret_list/list.txt", "a")
+            válasz.write(f'{nickname.capitalize()}: {partner},  {saturday},{" | ".join(date)}')
             válasz.write(new_line)
         clear()
 
@@ -185,7 +186,7 @@ def main():
         date = which_date()
         date_or_dates = date_iterator(date)
         appender(nickname, partner, date_or_dates, saturday)
-        print("\u001b[34;1mKöszönöm, hogy kitöltötted. Kérlek told vissza gitre :)\033[0m".center(middle()))
+        print("Köszönöm, hogy kitöltötted. Kérlek told vissza gitre :)".center(middle()))
 
     invite()
 
